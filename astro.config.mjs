9131a053f258650508eefd6d99d479d7d6808c4f
gx <gaox1@qq.com>
2025-12-17 12:40:46 +0800

    fix back to home path to import.meta.env.BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 // 添加 MDX 支持以处理文章
 export default defineConfig({
   site: 'https://gao20250409.github.io',
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-blog/' : '/',
   integrations: [
     mdx(),
   ],
