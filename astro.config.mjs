0fb01f094a99a8ce99057f78ac52d105026bb840
gx <gaox1@qq.com>
2025-12-31 14:25:59 +0800

    fix: remove base path for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import mdx from '@astrojs/mdx';
 
 // 添加 MDX 支持以处理文章
 export default defineConfig({
+  site: 'https://gao20250409.github.io',
+  base: process.env.NODE_ENV === 'production' ? '/my-blog/' : '/',
   integrations: [
     mdx(),
   ],
